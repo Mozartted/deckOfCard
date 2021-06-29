a937cd49a2f6929fa6de23ddf222843a53dd2102
Chibuike Emmanuel Osita <mozart.osita@gmail.com>
2021-06-29 13:34:10 +0100

    updated changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Container, Row, Col, Button} from "react-bootstrap"
 import {importAll} from "./utils"
 import {Card} from "./components"
+import {PenTool} from "react-feather"
 
 
 const deck = importAll(require.context('./assets/deckOfCards', false, /\.(png|jpe?g|svg)$/));
@@ -13,6 +14,10 @@ function App() {
 
   const [deckOfCards, updateDeckOfCards] = useState(deckObjects)
   const [pickedDeck, updatePickedDeck] = useState([])
+
+  useEffect(() => {
+    resetDeck()
+  }, [])
 
   const resetDeck = () => {
     updateDeckOfCards(deckObjects)
@@ -26,7 +31,6 @@ function App() {
       for(let i = 0; i< 5; i++){
         let randomIndex = Math.floor(Math.random()*items.length);
         pluckedList.push(items[randomIndex])
-        // remove item at index from a list.
         items.splice(randomIndex, 1)
       }
       updateDeckOfCards(items)
@@ -35,33 +39,35 @@ function App() {
       alert(`There are only ${deckOfCards.length} items left on the list, please reset`)
     }
   }
+
   return (
     <Container fluid>
       <Row className="justify-content-center">
         <Col md={5} className="text-center mt-4">
           <h2>Deck of Cards</h2>
-          <p>Pick some random cards</p>
+          <p className="">Pick some random cards</p>
         </Col>
       </Row>
       <Row className="justify-content-center">
-        <Col md={5} className="text-center">
+        <Col md={4} className="text-center">
           <Row>
-            <Col col={6}>
-              <Button size="sm" className="mr-2" onClick={pickFiveRandomCards}>
+              <Button block className="mb-2" onClick={pickFiveRandomCards}>
                 Pick 5 random cards
               </Button>
-            </Col>
-            <Col col={6}>
-              <Button size="sm" variant="secondary" onClick={resetDeck}>
+              <Button block variant="outline-primary" onClick={resetDeck}>
                 Reset the deck
               </Button>
-            </Col>
+            {/* <Col md={6}>
+             
+            </Col> */}
           </Row>
         </Col>
       </Row>
+      <hr/>
       <Row className="mt-5">
-        <Col md={6}>
-          <h3>Original Deck</h3>
+        <Col md={6} sm={12}>
+          <h4>Original Deck</h4>
+          <hr/>
           <Row>
             {
               deckOfCards.map((card, index) => {
@@ -70,8 +76,9 @@ function App() {
             }
           </Row>
         </Col>
-        <Col md={6}>
-          <h3>Picked Cards</h3>
+        <Col md={6} sm={12}>
+          <h4>Randomly Picked Cards</h4>
+          <hr/>
           <Row>
             <>
               {
@@ -84,7 +91,7 @@ function App() {
                     }
                 </>:
                 <Col className="text-center">
-                  <h4>No cards picked </h4>
+                  <p>No cards picked </p>
                 </Col>
               }
             </>
